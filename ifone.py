f6b3be963f6cba5943ca8b0f92b411db6df8a477
Uma <getumasaravanan@gmail.com>
2022-03-12 14:58:23 -0500

    commit

---
+++ ifone.py
@@ -0,0 +1,21 @@
+from itertools import count
+
+#print 1---->5 times using 'if'
+count=1
+if count<=5:
+    print(1)
+    count=count+1
+if count<=5:
+    print(1)
+    count=count+1
+if count<=5:
+    print(1)
+    count=count+1
+if count<=5:
+    print(1)
+    count=count+1
+if count<=5:
+    print(1)
+    count=count+1
+              
+
