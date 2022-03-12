4378195af95c205718072bd08b0172b1cdc04400
Uma <getumasaravanan@gmail.com>
2022-03-12 14:38:28 -0500

    second commit

---
+++ compare-threenos.py
@@ -0,0 +1,11 @@
+#comparing three number
+no1=10
+no2=20
+no3=30
+if no1>no2 and no1>no3:
+    print("biggest no",no1)
+elif no2>no1 and no2>no3:
+    print("biggset no", no2)
+else:
+    print("biggest no",no3)
+        
