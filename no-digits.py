2002287a3961d5ab833611e658f449af754ff01d
Uma <getumasaravanan@gmail.com>
2022-03-16 15:57:07 -0400

    commit

--- no-digits.py
+++ no-digits.py
@@ -6,3 +6,13 @@ while no>0:
     no=no//10
     count=count+1
 print(count)
+
+# Addition of Digits
+no=1234
+total=0
+while no>0:
+    rem=no%10
+    total=total+rem
+    no=no//10
+print(total)
+    
