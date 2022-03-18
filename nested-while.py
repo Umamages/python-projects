49b7e502bc90970ed6c08f1e5ed703579035cb92
Uma <getumasaravanan@gmail.com>
2022-03-17 20:30:33 -0400

    commit

---
+++ nested-while.py
@@ -0,0 +1,8 @@
+no=1
+while no<=3:
+    i=0
+    while i<=5:
+         print(no,end=' ')
+         i=i+1
+    print()
+    no=no+1
--- test1.py
+++ test1.py
@@ -1,3 +1,13 @@
-a=10
-b=20
-print(a+b)
+no1=int(input("enter no:"))
+no2=int(input("enter no:"))
+small = no1 if no1<no2 else no2
+day=1
+last= 0
+while day<=small:
+  if no1%day == 0 and no2%day == 0:
+    last = day
+  day+=1
+if last!=0:
+  print(last)
+else:
+  print("no common divisor") 
